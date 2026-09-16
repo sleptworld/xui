4a773838a07744b3855e9b345c6e24a654112726
sleptworld <qwin7989@gmail.com>
2026-09-17 00:44:17 +0800

    Re-export MacRunnerOptions from xui_macos

--- xui/src/lib.rs
+++ xui/src/lib.rs
@@ -1,4 +1,4 @@
 pub use xui_core::prelude::*;
 #[cfg(target_os = "macos")]
-pub use xui_macos::runner;
+pub use xui_macos::{MacRunnerOptions, runner};
 pub use xui_macros::{component, main, style, xui};
